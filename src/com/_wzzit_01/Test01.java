0164672e889bcd106168e13377da723a5e5b7c79
weizhuangzhuang <694678402@qq.com>
2018-09-12 10:41:56 +0800

    commit

---
+++ src/com/_wzzit_01/Test01.java
@@ -0,0 +1,20 @@
+package com._wzzit_01;
+
+import org.junit.Test;
+
+/**
+ * 
+ * <p>Copyright: Copyright (c) 2018</p>
+ * <p>succez</p>
+ * @author wzz
+ * @createdate 2018年9月12日
+ */
+public class Test01 {
+	@Test
+	public void fun() {
+
+		System.out.println("hello");
+
+	}
+
+}
